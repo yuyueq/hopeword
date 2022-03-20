9257384accef7aaee5dbbbaded798be7d963df51
yuyueq <3289705398@qq.com>
2022-03-20 17:11:56 +0800

    初始化vuepress-hope

---
+++ yuyueq/.vuepress/.temp/pages/.~README.html.js
@@ -0,0 +1,59 @@
+export const data = {
+  "key": "v-21564982",
+  "path": "/.~README.html",
+  "title": "",
+  "lang": "zh-CN",
+  "frontmatter": {
+    "summary": "",
+    "head": [
+      [
+        "meta",
+        {
+          "property": "og:url",
+          "content": "https://vuepress-theme-hope-v2-demo.mrhope.site/.~README.html"
+        }
+      ],
+      [
+        "meta",
+        {
+          "property": "og:site_name",
+          "content": "主题演示"
+        }
+      ],
+      [
+        "meta",
+        {
+          "property": "og:type",
+          "content": "article"
+        }
+      ],
+      [
+        "meta",
+        {
+          "property": "og:locale",
+          "content": "zh-CN"
+        }
+      ]
+    ]
+  },
+  "excerpt": "",
+  "headers": [],
+  "readingTime": {
+    "minutes": 0,
+    "words": 0
+  },
+  "filePathRelative": ".~README.md"
+}
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
